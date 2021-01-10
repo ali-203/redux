dbe90e50a7c3e2f0036d35c88c2c36331b767a5f
ali-203 <62180010+ali-203@users.noreply.github.com>
2021-01-10 22:12:15 +0500

    updated redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore();
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root')
 );
 
 
